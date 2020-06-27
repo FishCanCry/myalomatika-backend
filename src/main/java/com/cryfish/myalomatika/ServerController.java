925e18b3f855e59d4a7b112a521d60c835081a2f
Pavel Pecherskih <pecherskih@netcracker.com>
2020-06-28 00:42:11 +0400

    CrossOrigin annotation was added to specify that calls will be made to this controller from different domains. In our case we have specified that a call can be made form localhost:4200

--- src/main/java/com/cryfish/myalomatika/ServerController.java
+++ src/main/java/com/cryfish/myalomatika/ServerController.java
@@ -1,9 +1,10 @@
 package com.cryfish.myalomatika;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ServerController {
 
