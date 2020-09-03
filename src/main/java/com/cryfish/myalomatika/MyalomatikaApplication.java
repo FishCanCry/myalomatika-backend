f2f279d7707d810aab0689bff2a2012e86c1827c
bernatskyi <bernatskyi@haulmont.com>
2020-09-03 21:05:35 +0400

    test commit

--- src/main/java/com/cryfish/myalomatika/MyalomatikaApplication.java
+++ src/main/java/com/cryfish/myalomatika/MyalomatikaApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyalomatikaApplication {
 
 	public static void main(String[] args) {
+		//test
+
 		SpringApplication.run(MyalomatikaApplication.class, args);
 	}
 
